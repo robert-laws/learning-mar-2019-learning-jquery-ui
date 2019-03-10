78eef574273c94339a82437f8017d86c69e83d89
Robert Laws <roblaws@me.com>
2019-03-10 19:00:56 -0400

    finish drag and drop

--- src/js/behaviors-and-effects.js
+++ src/js/behaviors-and-effects.js
@@ -12,7 +12,10 @@ $(document).ready(function() {
   $("#resize3").resizable({
     grid: [
       20, 10
-    ]
+    ],
+    start: function(evt, ui) {
+      $(this).find("h2").text("Resizing...");
+    }
   });
 
   $("#resize4").resizable({
@@ -30,4 +33,35 @@ $(document).ready(function() {
       $(this).text("w: " + Math.round(ui.element.width()) + ", h: " + Math.round(ui.element.height()));
     }
   })
+});
+
+// Drag and Drop
+$(document).ready(function() {
+  $("#drag1").draggable();
+
+  $("#drag2").draggable({
+    axis: "x",
+    handle: "h2",
+    revert: true
+  });
+
+  $("#drag3").draggable({
+    handle: "h2"
+  });
+
+  $("#dropArea").droppable({
+    accept: "#drag3",
+    hoverClass: "dragHighlight",
+    tolerance: "pointer", // options - fit, pointer, intersect, touch
+    activate: function(evt, ui) {
+      $(this).find("h2").css("background-color", "cornsilk");
+    },
+    deactivate: function(evt, ui) {
+      $(this).find("h2").css("background-color", "");
+    },
+    drop: function(evt, ui) {
+      $(this).find("h2").text("Dropped...");
+      ui.draggable.find("h2").text("Item Dropped");
+    }
+  });
 });
